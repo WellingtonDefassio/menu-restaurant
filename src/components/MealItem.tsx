310818c0a87fe85d31d8abfc26784395cac427a7
WellingtonDefassio <well.defassio@gmail.com>
2023-12-19 21:43:32 -0300

    create a mealItem component

--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, Image, StyleSheet} from 'react-native';
 import Meal from "../models/meal";
 
 interface MealItemProps {
     meal: Meal
 }
 
-export default function MealItem({ meal }: MealItemProps) {
+export default function MealItem({meal}: MealItemProps) {
     return (
-        <View>
-            <Text>{meal.title}</Text>
+        <View style={styles.mealItem}>
+            <Pressable
+                android_ripple={{color: "#ccc"}}
+                style={({pressed}) => pressed ? styles.buttonPressed : null}>
+                <View style={styles.innerContainer}>
+                    <View>
+                        <Image source={{uri: meal.imageUrl}} style={styles.image}/>
+                        <Text style={styles.title}>{meal.title}</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.detailItem}>{meal.duration}m</Text>
+                        <Text style={styles.detailItem}>{meal.complexity.toUpperCase()}</Text>
+                        <Text style={styles.detailItem}>{meal.affordability.toUpperCase()}</Text>
+                    </View>
+                </View>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: "hidden",
+        backgroundColor: "white",
+        elevation: 6,
+        shadowColor: "black",
+        shadowOpacity: 0.25,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: "hidden"
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 18,
+        margin: 8
+    },
+    details: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 8
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12
+    },
+    buttonPressed: {
+        opacity: 0.5,
+    }
+})
